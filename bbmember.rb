10909712dff55e66c23e3e0059a0e308d1b5ea01
Noel Cower <ncower@gmail.com>
2009-03-23 12:52:10 -0700

    Added documentation comments to BBMember.  Part of an ongoing process to make code slightly less mindfuck Added setters for isPrivate, isExtern, page, name, etc. - anything there should have a setter.  Most setters are protected. initialize now throws an exception if it's called and the type being created is a BBMember and not an extending class. Added typeRef method to get a reference to the type that the member is of if its type was found in any source pages.

--- bbmember.rb
+++ bbmember.rb
@@ -17,9 +17,18 @@
 require "regexps.rb"
 require "sourcepage.rb"
 
+
+# Base class for BlitzMax members.  These may be global variables, methods,
+# functions, or any entity other than a Type (BBType handles that).
+# 
+# This class should not be used by itself.
 class BBMember
+	
 	include BBRegex
 	
+	# Map of type shortcuts.
+	# 
+	# These are used to translate shortcuts into their long type names.
 	TYPE_SHORTCUTS={
 		"@"  => "Byte",
 		"%"  => "Int",
@@ -31,51 +40,164 @@ class BBMember
 		"$w" => "WString"
 	}
 	
-	def initialize()
-		raise "Not implemented"
+	### Processing
+	
+	# Base initialize method.  Sets the name, type, and page instance
+	# variables.  This should not be called on its own.
+	def initialize(name, type, page)
+		raise "Cannot create instance of BBMember" if self.class == BBMember
+		
+		self.name=name
+		self.type=type
+		self.page=page
+		
+		@typeRef = nil
+		
+		@isExtern = false
+		@isPrivate = false
+		
+		@startingLineNumber = nil
+		@endingLineNumber = nil
+		
+		@documentation = nil
 	end
 	
+	# Base process method.
 	def process
-		raise "Not implemented"
 	end
 	
+	protected :initialize
+	protected :process
+	
+	
+	### Basic information
+	
+	# Returns the name of the member.
 	def name
 		@name
 	end
 	
+	# Sets the name of the member.
+	def name=(name)
+		@name = name
+	end
+	protected :name=
+	
+	# Returns a string representing the type of the member.
 	def type
 		@type
 	end
 	
+	# Sets the type of the member.
+	def type=(type)
+		@type = type
+	end
+	protected :type=
+	
+	# Returns the page the member belongs to.
 	def page
 		@page
 	end
 	
+	# Sets the page that the member belongs to.
+	def page=(page)
+		@page = page
+	end
+	protected :page=
+	
+	# Returns whether or not the member is inside of an Extern block.
 	def extern?
 		@isExtern
 	end
 	
+	# Sets whether or not the member is inside of an Extern block.
+	def isExtern=(isExtern)
+		@isExtern = isExtern
+	end
+	protected :isExtern=
+	
+	# Returns whether or not the member is private.
 	def private?
 		@isPrivate
 	end
 	
+	# Sets whether or not the member is private.
+	def isPrivate=(isPrivate)
+		@isPrivate = isPrivate
+	end
+	protected :isPrivate=
+	
+	# Returns whether or not the member is public.  The is the inverse of private?
+	def public?
+		!self.private?
+	end
+	
+	
+	### Line numbers
+	
+	# Returns the line that the member starts on.
 	def startingLineNumber
 		@startingLineNumber
 	end
 	
+	# Sets the line that the member ends on.
+	def startingLineNumber=(line)
+		@startingLineNumber = line
+	end
+	protected :startingLineNumber=
+	
+	# Returns the line that the member ends on.
 	def endingLineNumber
 		@endingLineNumber
 	end
 	
+	# Sets line that the member ends on.
+	def endingLineNumber=(line)
+		@endingLineNumber = line
+	end
+	protected :endingLineNumber=
+	
+	# Returns a Range for startingLineNumber to endingLineNumber, inclusive.
+	def lineRange
+		Range.new(@startingLineNumber, @endingLineNumber, false)
+	end
+
+
+	### Documentation
+
+	# Returns a reference to a BBDoc object if one is associated with the member, otherwise nil.
 	def documentation
 		@documentation
 	end
 	
+	# Sets the member's documentation object to doc.  Must be a BBDoc.
 	def documentation=(doc)
+		raise "Documentation object is not a BBDoc" unless doc.nil? or doc.is_a?(BBDoc)
 		@documentation = doc
 	end
 	
+	
+	### Types
+	
+	# Returns the type of member this is.
 	def memberType
 		raise "Not implemented"
 	end
+	
+	# Returns a reference to a BBType if one is found that matches the member's type.  The result is cached.
+	def typeRef
+		if self.type.nil? then
+			@typeRef = nil
+		elsif @typeRef.nil? then
+			typeName = self.type[/^[a-zA-Z_]\w*/,0]
+			# don't even bother checking for the primitive types
+			return nil if typeName =~ /(?i)(object|string|byte|short|int|long|float|double|cstring|wstring)/
+			
+			cls = BBType.getClass(typeName)
+			@typeRef = cls unless cls.nil?
+		end
+		
+		return @typeRef
+	end
+	
 end
