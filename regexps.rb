1f40660b629cad57e5d548fd5a1a291a70ff995d
Noel Cower <ncower@gmail.com>
2009-03-24 13:16:57 -0700

    Added documentation for regexps.

--- regexps.rb
+++ regexps.rb
@@ -14,11 +14,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Regex constants
 
+#################################  BBRegex  ##################################
+
+
+
+# A set of regular expressions to match important elements of BlitzMax source
+# code.
 module BBRegex
+	
+	# Regex to match lines that specify the visibility of following entities.
 	VISIBILITY_REGEX=/(?i)^(?:(?<private>private)|(?<public>public))$/
 	
+	# Regex to match the beginning of Types.
 	TYPE_REGEX=/(?ix)^
 		# type and name
 		type \s+ (?<name>[a-zA-Z_]\w*)
@@ -30,40 +38,46 @@ module BBRegex
 		(?: \s* \{(?<attributes> .*)\})?
 	$/
 	
+	# Regex to match the ending of types.
 	TYPE_END_REGEX=/(?i)^end\s?type$/
 	
+	# Regex to match the beginning of methods.
 	METHOD_REGEX=/(?ix)^
 		(?: method \s+ (?<name> [a-zA-Z_]\w* ) )
 		(?: \s* (?<returntype> [^\(]* ) )
 		(?: \( (?<arguments> .* ) \) )
-		(?: \s* (?<abstract>abstract)|(?<final>final) )?
-		(?: \s* \{(?<attributes> .*)\} )?
+		(?: \s* (?<abstract>abstract) | (?<final>final) )?
+		(?: \s* \{ (?<attributes> .*) \} )?
 	$/
 	
+	# Regex to match the beginning of function.
 	FUNCTION_REGEX=/(?ix)^
 		function \s+ (?<name>[a-zA-Z_]\w*) \s* (?<returntype>[^\(]*) \( (?<arguments>.*) \)
 		(?:\s*"(?<callingConv>[^"])*")
 		(?: \s* \{(?<attributes> .*)\} )?
 	$/
 	
+	# Regex to match the beginning of an Extern block.
 	EXTERN_REGEX=/(?ix)^
 		extern (?:\s+ (?<extern>"[^"]+"))?
 	$/
 	
+	# Regex to match extern functions.
 	EXTERN_FUNCTION_REGEX=/(?ix)^
 		function \s+ (?<name>[a-zA-Z_]\w*) \s* (?<returntype>[^\(]*) \( (?<arguments>.*) \)
 		(?:\s*"(?<callingConv>[^"]*)")?
 		(?:\s*="(?<externName>[^"]*)")?
 	$/
 	
+	# Regex to match the end of an Extern block.
 	EXTERN_END_REGEX=/(?ix)^end\s?extern$/
 	
-	#
+	# Regex to match the end of functions and methods.
 	METHOD_END_REGEX=/(?i)^end\s?(method|function)$/
 	
 	# variables
 	
-	## comma-separated, parse names/values separately
+	# Matches one or more globals, fields, or constants.
 	VARIABLE_REGEX=/(?ix)^
 		(?<membertype>const|global|field) \s+ (?<values>.+?)
 		(?: \s* \{(?<attributes> .*)\} )?
@@ -71,6 +85,7 @@ module BBRegex
 	
 	# type specifiers
 	
+	# Matches a type specifier. e.g. <tt>:Object[] Var</tt> or <tt>@ Ptr</tt>
 	TYPENAME_REGEX=/(?ix)
 			# type shortcut
 		(?:
@@ -88,22 +103,29 @@ module BBRegex
 	
 	# value (name[type][ = value])
 	
+	# Matches a variable name, type, and value.  E.g., <tt>foo:Int = 50</tt>
 	VALUE_REGEX=/(?ix)^
 		(?<name>[a-zA-Z_]\w*)
 		\s* (?<typename>#{TYPENAME_REGEX}(?:\(.*?\))?)
 		(?:\s* = \s*(?<value>.+))?
 	$/
 	
+	# Matches the beginning of a documentation comment block.
 	DOC_REGEX=/(?ix)^
 		(?i)\b(?<!end|end\s)
 		rem:doc(?:\s+(.+))?
 	$/
 	
+	# Matches a tag in a documentation block.
 	DOC_TAG_REGEX=/(?ix)^@(?<name>\w+)(?:\:(?<key>\w+|"[^"]*"))?(?:\s+(?<body>.+))?$/
 	
+	# Matches a regular block comment.
 	REM_REGEX=/(?i)\b(?<!end|end\s)rem(?!\:doc)\b/
 	
+	# Matches the end of a block comment.
 	REM_END_REGEX=/(?ix)\bend\s?rem\b/
 	
+	# Matches a block comment that begins and ends in the middle of a line.
 	REM_MIDLINE_REGEX=/(?ix)\b(?<!end|end\s)rem\b.*?\bend\s?rem\b/
+	
 end
