db12f5d0323ff4dd429592db881fc18565c34f4a
Noel Cower <ncower@gmail.com>
2009-03-24 13:14:41 -0700

    Added documentation for class/methods. An exception is thrown if the memberType passed to the constructor is nil.

--- bbvar.rb
+++ bbvar.rb
@@ -14,19 +14,40 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 require "regexps.rb"
 require "sourcepage.rb"
 require "bbmember.rb"
 
+
+###################################  BBVar  ##################################
+
+
+
+# Class to describe variables in source pages, method arguments, and types.
 class BBVar < BBMember
+	
+	# Initializes a BBVar.
+	# 
+	# value is the string specifying the name, type, and optionally the value
+	# of the variable.  E.g.,
+	# 
+	# 	var = BBVar.new("foo:Int = 50", 0, nil, "global", false, false)
+	# 	puts var.name			# => foo
+	# 	puts var.type			# => Int
+	# 	puts var.value		# => 50
+	# 	puts var.memberType	# => global
 	def initialize(value, lineNumber, page, memberType, isExtern, isPrivate)
 		md = VALUE_REGEX.match(value)
 		
 		if (type = md[:typename]).nil? then
+			# No type specified
 			type = "Int"
 		elsif md[:fulltype] then
+			# Full type specified
 			type.slice!(/^:\s*/)
 		elsif shortcut = md[:shortcut] then
+			# Shortcut specified (e.g., % for Int)
 			type[0,shortcut.length] = TYPE_SHORTCUTS[shortcut]
 		end
 		
@@ -39,12 +60,19 @@ class BBVar < BBMember
 		self.isPrivate = isPrivate
 		
 		@memberType = memberType
+		
+		if memberType.nil? then
+			raise "Type of member cannot be nil"
+		end
 	end
 	
+	# Method to process contents of a variable - does nothing, as variables have no contents per se.
 	def process
 		return
 	end
 	
+	# Gets the type of member this variable is.  E.g., global, field, or
+	# const.
 	def memberType
 		@memberType
 	end
@@ -58,7 +86,10 @@ class BBVar < BBMember
 		return outs
 	end
 	
+	# Get the value of the variable.  Returns a String if the variable has a
+	# value, otherwise it returns nil.
 	def value
 		@value
 	end
+	
 end
